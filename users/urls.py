29f494f5f99745add7ae9003d071f32782ec9bf8
Jon Brennan <jonbrennan@Jons-MacBook-Pro.local>
2023-11-06 11:21:10 -0500

    update check

--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,5 @@ from users import views
 urlpatterns = [
     path("", include("djoser.urls")),
     path("", include("djoser.urls.authtoken")),
+    path("api-token-auth/", views.CustomAuthToken.as_view()),
 ]
